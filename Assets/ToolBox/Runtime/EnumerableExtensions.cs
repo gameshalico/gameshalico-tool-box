5a3c9d2593e6c712be6930b014bce5faa22ef458
gameshalico <gameshalico@outlook.jp>
2023-03-30 00:27:28 +0900

    Enumerableのforeach拡張メソッドの追加

---
+++ Assets/ToolBox/Runtime/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+
+namespace Shalico.ToolBox
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> action)
+        {
+            foreach(T item in sequence)
+                action(item);
+        }
+    }
+}
