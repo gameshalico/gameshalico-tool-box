92b34b5f5b6efd3f76794ac03bbc36a99627ea40
gameshalico <gameshalico@outlook.jp>
2023-06-12 23:05:12 +0900

    Package Importerをカスタム可能に

--- Assets/ShalicoToolBox/Editor/PackageImporter.cs
+++ Assets/ShalicoToolBox/Editor/PackageImporter.cs
@@ -1,66 +1,105 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace Shalico.ToolBox.Editor
 {
     public class PackageImporter : EditorWindow
     {
-        private static readonly List<PackageData> s_packages = new()
-        {
-            new PackageData
-            {
-                Identifier = "https://github.com/neuecc/UniRx.git?path=Assets/Plugins/UniRx/Scripts",
-                ShouldImport = true
-            },
-            new PackageData
-            {
-                Identifier = "https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask",
-                ShouldImport = true
-            },
-            new PackageData
-            {
-                Identifier = "https://github.com/hadashiA/VContainer.git?path=VContainer/Assets/VContainer",
-                ShouldImport = true
-            }
-        };
+        private static readonly string s_packagePrefsKey = "PackageImporter.PackageData";
 
-        private void OnEnable()
-        {
-            Debug.Log("Package Importer Enabled");
-        }
+        private static List<PackageData> s_packages;
+        private string _newPackageIdentifier = "";
+        private string _newPackageName = "";
 
         private void OnGUI()
         {
             foreach (PackageData package in s_packages)
             {
-                package.ShouldImport = EditorGUILayout.ToggleLeft(package.Identifier, package.ShouldImport);
+                EditorGUILayout.BeginHorizontal();
+                package.shouldImport = EditorGUILayout.ToggleLeft(package.name, package.shouldImport);
+                if (GUILayout.Button("Delete"))
+                {
+                    s_packages.Remove(package);
+                    SavePackageData();
+                }
+
+                EditorGUILayout.EndHorizontal();
             }
 
+            GUILayout.Space(20);
+            GUILayout.Label("Add New Package", EditorStyles.boldLabel);
+            _newPackageName = EditorGUILayout.TextField("Name", _newPackageName);
+            _newPackageIdentifier = EditorGUILayout.TextField("Identifier", _newPackageIdentifier);
+            if (GUILayout.Button("Add Package"))
+            {
+                s_packages.Add(new PackageData(_newPackageName, _newPackageIdentifier, false));
+                _newPackageName = "";
+                _newPackageIdentifier = "";
+                SavePackageData();
+            }
+
+            GUILayout.Space(20);
             if (GUILayout.Button("Import Selected Packages"))
             {
+                ListRequest listRequest = Client.List();
+
+                while (!listRequest.IsCompleted)
+                {
+                }
+
+                List<string> installedPackages = new();
+                foreach (PackageInfo package in listRequest.Result)
+                {
+                    installedPackages.Add(package.name);
+                }
+
                 foreach (PackageData package in s_packages)
                 {
-                    if (package.ShouldImport)
+                    if (package.shouldImport && !installedPackages.Contains(package.identifier))
                     {
-                        ImportPackage(package.Identifier);
+                        ImportPackage(package.identifier);
                     }
                 }
             }
         }
 
-
         [MenuItem("Window/Package Importer")]
         public static void ShowWindow()
         {
             GetWindow<PackageImporter>("Package Importer");
+            LoadPackageData();
         }
 
-        private void ImportPackage(string identifier)
+        private static void LoadPackageData()
         {
-            AddRequest request = Client.Add(identifier);
+            string jsonData = EditorPrefs.GetString(s_packagePrefsKey, "");
+
+            if (string.IsNullOrEmpty(jsonData))
+            {
+                s_packages = new List<PackageData>();
+            }
+            else
+            {
+                s_packages = JsonUtility.FromJson<ListWrapper<PackageData>>(jsonData).list;
+            }
+        }
+
+        private static void SavePackageData()
+        {
+            ListWrapper<PackageData> wrapper = new() { list = s_packages };
+
+            string jsonData = JsonUtility.ToJson(wrapper);
+            EditorPrefs.SetString(s_packagePrefsKey, jsonData);
+        }
+
+        private void ImportPackage(string packageName)
+        {
+            AddRequest request = Client.Add(packageName);
 
             while (!request.IsCompleted)
             {
@@ -72,14 +111,29 @@ namespace Shalico.ToolBox.Editor
             }
             else if (request.Status >= StatusCode.Failure)
             {
-                Debug.Log("Failed to install package: " + identifier);
+                Debug.Log("Failed to install package: " + packageName);
             }
         }
     }
 
-    public record PackageData
+    [Serializable]
+    public class PackageData
     {
-        public string Identifier { get; init; }
-        public bool ShouldImport { get; set; }
+        public string name;
+        public string identifier;
+        public bool shouldImport;
+
+        public PackageData(string name, string identifier, bool shouldImport)
+        {
+            this.name = name;
+            this.identifier = identifier;
+            this.shouldImport = shouldImport;
+        }
+    }
+
+    [Serializable]
+    public class ListWrapper<T>
+    {
+        public List<T> list;
     }
 }
