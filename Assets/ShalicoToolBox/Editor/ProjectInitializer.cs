946353b995293052f7310372a6a914db81638d98
gameshalico <gameshalico@outlook.jp>
2023-06-12 22:38:30 +0900

    .gitkeepを生成するように修正

--- Assets/ShalicoToolBox/Editor/ProjectInitializer.cs
+++ Assets/ShalicoToolBox/Editor/ProjectInitializer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
@@ -10,41 +8,32 @@ namespace Shalico.ToolBox.Editor
     {
         private static string s_projectName = "ProjectName";
 
-        private string[] _folders = new string[] {
-            "Art/Materials",
-            "Art/Models",
-            "Art/Textures",
-            "Art/Animations",
-            "Art/Animators",
-            "Art/Fonts",
-
-            "Audio/Music",
-            "Audio/SFX",
-
-            "Code/Scripts/Core/Managers",
-            "Code/Scripts/Core/Utilities",
-            "Code/Scripts/GamePlay",
-            "Code/Scripts/UI",
-
-            "Code/Shaders",
-            "Code/Particles",
-
-            "Editor/Scripts",
-
-            "Levels/Scenes",
-            "Levels/Prefabs",
-            "Levels/UI",
-            "Levels/ScriptableObjects",
-
-            "Settings",
-            "Documents"
+        private readonly string[] _folders =
+        {
+            "Art/Materials", "Art/Models", "Art/Textures", "Art/Animations", "Art/Animators", "Art/Fonts",
+            "Audio/Music", "Audio/SFX", "Code/Scripts", "Code/Shaders", "Code/Particles", "Editor/Scripts",
+            "Levels/Scenes", "Levels/Prefabs", "Levels/UI", "Levels/ScriptableObjects", "Settings", "Documents"
         };
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("Project Initializer");
+            s_projectName = EditorGUILayout.TextField("Project Name", s_projectName);
+
+            GUILayout.Space(10);
+            if (GUILayout.Button("Initialize"))
+            {
+                CreateFolders();
+                ScriptBatchCreator.GenerateAll();
+            }
+        }
 
         private void CreateFolders()
         {
             _folders.ForEach(folder =>
             {
                 Directory.CreateDirectory($"{Application.dataPath}/{s_projectName}/{folder}");
+                File.Create($"{Application.dataPath}/{s_projectName}/{folder}/.gitkeep");
             });
             AssetDatabase.Refresh();
         }
@@ -53,18 +42,6 @@ namespace Shalico.ToolBox.Editor
         private static void Open()
         {
             GetWindow<ProjectInitializer>("Project Initializer");
-        }
-
-        private void OnGUI()
-        {
-            EditorGUILayout.LabelField("Project Initializer");
-            s_projectName = EditorGUILayout.TextField("Project Name", s_projectName);
-            GUILayout.Space(10);
-            if (GUILayout.Button("Initialize"))
-            {
-                CreateFolders();
-                ScriptBatchCreator.GenerateAll();
-            }
         }
     }
 }
