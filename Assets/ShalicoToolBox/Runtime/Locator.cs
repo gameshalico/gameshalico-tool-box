72faaa7c723cfa2d91554ce208552b5e6fedb7e0
gameshalico <gameshalico@outlook.jp>
2023-10-15 16:41:29 +0900

    サービスロケータの実装

---
+++ Assets/ShalicoToolBox/Runtime/Locator.cs
@@ -0,0 +1,19 @@
+﻿namespace ShalicoToolBox
+{
+    public class Locator<T> where T : class
+    {
+        public static T Instance { get; private set; }
+
+        public static bool IsValid => Instance != null;
+
+        public static void Bind(T instance)
+        {
+            Instance = instance;
+        }
+
+        public static void Unbind()
+        {
+            Instance = null;
+        }
+    }
+}
